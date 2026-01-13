66123401dd666452cddcb07713b181bc265597bf
partoftheworlD <partoftheworld@vivaldi.net>
2026-01-13 03:21:22 +0300

    Added support for IDA 9 SDK with minor optimizations

--- functionstringassociate.py
+++ functionstringassociate.py
@@ -3,106 +3,84 @@ import idautils
 import idaapi
 import ida_bytes
 
-print("\nStringsFunctionAssociate v0.09 by partoftheworlD! Last Changes <2024-01-15 19:50:02.814613>\n")
-
 class StringException(Exception):
     pass
 
-class Strings_fc:
-    def __init__(self):
-        self.string_counter = 0
-
-    string_types = ["C",
-                    "Pascal",
-                    "LEN2",
-                    "Unicode",
-                    "LEN4",
-                    "ULEN2",
-                    "ULEN4"]
-
-    def get_string_type(self, addr):
-        try:
-            type_s = idc.get_str_type(addr)
-            return str(ida_bytes.get_strlit_contents(addr, -1, type_s))
-        except TypeError:
-            raise StringException()        
-    
-    def clear_comments(self, func_obj):
-        idaapi.set_func_cmt(func_obj, '', 1)
-        idaapi.set_func_cmt(func_obj, '', 0)
-
-    def save_existing_comments(self, start_func):
-        bc = []
-        comment = idaapi.get_func_cmt(start_func, 1)
-        if comment:
-            bc.append(comment)
-        comment = idaapi.get_func_cmt(start_func, 0)
-        if comment:
-            bc.append(comment)
-        if len(bc) > 1:
-            return bc
-        else:
+def get_string_at(addr):
+    try:
+        s_type = idc.get_str_type(addr)
+        if s_type == -1 or s_type is None:
             return None
 
-    def get_strings_per_function(self, start_func):
-        strings = []
-        fs = ''
-        func_obj = idaapi.get_func(start_func)  
-        if func_obj:
-            bc = self.save_existing_comments(start_func)
-            if bc is not None:
-                for string in [saved_comment for saved_comment in bc]:
-                    strings.append(string)
-            self.clear_comments(func_obj)
-            for inst_list in idautils.Heads(start_func, idc.find_func_end(start_func)):
-                try:
-                    for string in [self.get_string_type(xref_addr) for xref_addr in idautils.DataRefsFrom(inst_list)]:
-                        if len(string) > 2:
-                            strings.append(string[2:-1])
-                            self.string_counter += 1
-                        else:
-                            pass
-                except StringException:
-                    continue
-    
-            if strings:
-                for c in strings:
-                    if '\n' in c:
-                        c = c.replace('\n', '')
-                    fs += '"' + c + '" '
-                idaapi.set_func_cmt(func_obj, 'STR {}# {}'.format(len(strings), fs), 1)
-            
-        else:
-            print("func_obj return 0")
-            
+        s_bytes = ida_bytes.get_strlit_contents(addr, -1, s_type)
+        if s_bytes:
+            try:
+                return s_bytes.decode("utf-8", errors="ignore").strip()
+            except:
+                return None
+    except TypeError:
+        raise StringException()
 
-    def main(self):
-        print("\n[+]Launching...")
-        try:            
-            for i in idautils.Functions():
-                self.get_strings_per_function(i)
-            print("\n[+]Well done! Added {} strings in {} functions".format(self.string_counter, idaapi.get_func_qty()))
-        except KeyboardInterrupt:
-            print("\n[+]Ended by user")
+def save_existing_comments(func_ea):
+    bc = []
+    for cmt_type in [0, 1]:
+        comment = idc.get_func_cmt(func_ea, cmt_type)
+        if comment:
+            if not comment.startswith("STR"):
+                bc.append(comment)
+    return bc if bc else None
 
+def clear_comments(func_ea):
+    idc.set_func_cmt(func_ea, "", 0)
+    idc.set_func_cmt(func_ea, "", 1)
 
-class StringsHandler(idaapi.action_handler_t):
+def run():
+    print(
+        "\nStringsFunctionAssociate v0.12 by partoftheworlD! Last Changes <2026-01-13 20:51:12.814613>\n[+]Launching...\n"
+    )
+    count = 0
+    total = idaapi.get_func_qty()
+
+    for func_ea in idautils.Functions():
+        strings_in_func = []
+
+        coms = save_existing_comments(func_ea)
+        if coms:
+            strings_in_func.extend(coms)
+
+        clear_comments(func_ea)
+
+        f_end = idc.get_func_attr(func_ea, idc.FUNCATTR_END)
+        for head in idautils.Heads(func_ea, f_end):
+            for xref in idautils.DataRefsFrom(head):
+                s = get_string_at(xref)
+                if s and len(s) > 2:
+                    cs = s.translate(str.maketrans({"\n": " ", "\r": " "}))
+                    if cs not in strings_in_func:
+                        strings_in_func.append(cs)
+
+        if strings_in_func:
+            comments = " ".join([f'"{x}"' if " " in x else x for x in strings_in_func])
+            idc.set_func_cmt(func_ea, f"STR {len(strings_in_func)}# {comments}", 1)
+            count += len(strings_in_func)
+
+    print(f"[+]Well done! Added {count} strings in {total} functions")
+
+class Handler(idaapi.action_handler_t):
     def __init__(self):
         idaapi.action_handler_t.__init__(self)
 
     def activate(self, ctx):
-        sfc = Strings_fc()
-        sfc.main()
+        run()
 
     def update(self, ctx):
         return idaapi.AST_ENABLE_ALWAYS
 
-
-class Strings_window(idaapi.plugin_t):
+class PluginButton(idaapi.plugin_t):
     flags = idaapi.PLUGIN_FIX
-    comment = 'Associate functions'
-    help = 'https://github.com/partoftheworlD/IDA7py_FunctionStringAssociate/'
-    wanted_name = 'Strings Function Associate'
+    comment = "Associate functions"
+    help = "https://github.com/partoftheworlD/IDApy_FunctionStringAssociate/"
+    wanted_name = "Strings Function Associate"
     wanted_hotkey = ""
 
     def init(self):
@@ -119,9 +97,8 @@ class Strings_window(idaapi.plugin_t):
         pass
 
     def run(self, arg=0):
-        str_Handle = StringsHandler()
-        str_Handle.activate(self)
-
+        h = Handler()
+        h.activate(self)
 
 def PLUGIN_ENTRY():
-    return Strings_window()
+    return PluginButton()
